becaaa9e88d5dc6ef2fb4baac9265f859964b6da
Mads <mads2097@gmail.com>
2019-09-02 13:07:23 +0200

    Create js25c.js

---
+++ Assignments/js25c.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log('Truth table for conjunction');
+console.log('var1\tvar2\tvar1 && var2');
+var var1 = true;
+var var2 = true;
+console.log((var1 + '\t' + var2) + '\t' + (var1 && var2));
+var var1 = true;
+var var2 = false;
+console.log((var1 + '\t' + var2) + '\t' + (var1 && var2));
+var var1 = false;
+var var2 = true;
+console.log((var1 + '\t' + var2) + '\t' + (var1 && var2));
+var var1 = false;
+var var2 = false;
+console.log((var1 + '\t' + var2) + '\t' + (var1 && var2));
