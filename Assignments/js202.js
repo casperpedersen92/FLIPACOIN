2f608459729f3dfcb00a6acd419545a342324feb
WhiteIronWolf <54797883+WhiteIronWolf@users.noreply.github.com>
2019-09-02 13:11:46 +0200

    Create js202.js

---
+++ Assignments/js202.js
@@ -0,0 +1,7 @@
+'use strict';
+var cpr = 1112950006; //cpr nummer for en kvinde
+console.log(cpr % 2 == 0); //true
+
+'use strict';
+var cpr = 1002960857; // cpr nummer for en mand
+console.log(cpr % 2 == 0); // false
